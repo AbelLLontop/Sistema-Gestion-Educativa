fff309d4264dab4ae26e0961a21236e2865975fa
Abel Gianmarco Llontop Meza <a.llontopmeza@gmail.com>
2024-07-05 22:48:02 -0500

    fix: se modifico el AreaService

--- src/services/area.service.ts
+++ src/services/area.service.ts
@@ -11,12 +11,21 @@ class AreaService {
     return area;
   }
   async updateArea(id:string, area: AreaCreateDto){
+    area.nombre = area.nombre.toUpperCase();
     const areaUpdated = await Area.findByIdAndUpdate(id, area, {new:true});
     return areaUpdated;
   }
   async createArea(area: AreaCreateDto){
+    area.nombre = area.nombre.toUpperCase();
     const newArea = new Area(area);
     return await newArea.save();
+  }
+  async createMultipleAreas(areas: AreaCreateDto[]){
+    areas = areas.map(area => {
+      area.nombre = area.nombre.toUpperCase();
+      return area;
+    });
+    return await Area.insertMany(areas);
   }
 }
 
